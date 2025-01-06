03634f685150ad4f95ab94d098915f319f13016f
webDevT <karinadesyatnik27@gmail.com>
2025-01-06 15:15:58 +0200

    mobile menu

--- app/js/main.js
+++ app/js/main.js
@@ -24,3 +24,41 @@ subItem.addEventListener('click', function (event) {
   event.preventDefault(); 
   subMenu.classList.toggle('active'); 
 });
+
+// Отслеживаем изменение состояния класса .active у .menu-wrapper
+
+
+
+
+
+
+// Флаг для отслеживания, был ли topHeader скрыт через кнопку .close
+let isManuallyClosed = false;
+
+// Скрываем topHeader при клике на .close
+closeButton.addEventListener('click', function () {
+  topHeader.style.display = 'none';
+  isManuallyClosed = true; // Устанавливаем флаг в true
+});
+
+// Обрабатываем логику показа/скрытия topHeader при клике на .menu-btn
+menuBtn.addEventListener('click', function () {
+  // Проверяем, есть ли у menuWrapper класс active
+  if (menuWrapper.classList.contains('active')) {
+    // Скрываем topHeader
+    topHeader.style.display = 'none';
+  } else if (!isManuallyClosed) {
+    // Если topHeader не был скрыт вручную, показываем его
+    topHeader.style.display = 'block';
+  }
+});
+
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 0) {
+    header.classList.add('sticky'); // Добавляем класс sticky, если страница прокручена
+  } else {
+    header.classList.remove('sticky'); // Убираем класс sticky, если страница вернулась в начало
+  }
+});
