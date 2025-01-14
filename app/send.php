eb05ca2953e1d00d9dd3b0519c6c8923a0a90149
webDevT <karinadesyatnik27@gmail.com>
2025-01-14 14:03:09 +0200

    updates

--- app/send.php
+++ app/send.php
@@ -1,18 +1,43 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $to = "karinadesyatnik27@gmail.com"; 
-    $subject = "New Contact Form Submission";
-    $message = "Name: " . $_POST["name"] . "\n" .
-               "Email: " . $_POST["email"] . "\n" .
-               "Message: " . $_POST["message"];
-    $headers = "From: " . $_POST["email"];
+    /*ПОМЕЩАЕМ ДАННЫЕ ИЗ ПОЛЕЙ В ПЕРЕМЕННЫЕ*/
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $text_comment = $_POST["text_comment"];
 
-    if (mail($to, $subject, $message, $headers)) {
-        // Если письмо отправлено, перенаправляем на страницу thank-you.html
-        header("Location: thank-you.html");
-        exit; 
-    } else {
-        echo "Error: Unable to send email.";
+    /*ЗДЕСЬ ПРОВЕРЯЕМ ЕСЛИ ХОТЯ БЫ ОДНО ИЗ ПОЛЕЙ НЕ ЗАПОЛНЕНО МЫ ВОЗВРАЩАЕМ СООБЩЕНИЕ*/
+    if($name=="" or $email=="" or $text_comment==""){ 
+        echo "<div>Заполните, пожалуйста, все поля</div>";
     }
-}
+
+
+    else{
+        /*ЕСЛИ ВСЕ ПОЛЯ ЗАПОЛНЕНЫ НАЧИНАЕМ СОБИРАТЬ ДАННЫЕ ДЛЯ ОТПРАВКИ*/
+        $to = "info@safeconsult.lv"; /* Адрес, куда отправляем письма*/
+        $subject = "safeconsult.lv"; /*Тема письма*/
+        $headers = "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/html; charset=utf-8\r\n";
+        $headers .= "From: <safeconsult.lv>\r\n";/*ОТ КОГО*/
+        
+
+        /*ВО ВНУТРЬ ПЕРЕМЕННОЙ $message ЗАПИСЫВАЕМ ДАННЫЕ ИЗ ПОЛЕЙ */
+        $message .= "Имя: ".$name."\n";
+        $message .= "Почта: ".$email."\n";
+        $message .= "Сообщение: ".$text_comment."\n";
+
+        /*ДЛЯ ОТЛАДКИ ВЫ МОЖЕТЕ ПРОВЕРИТЬ ПРАВИЛЬНО ЛИ ЗАПИСАЛИCM ДАННЫЕ ИЗ ПОЛЕЙ*/ 
+
+        $send = mail($to, $subject, $message, $headers);
+
+        /*ЕСЛИ ПИСЬМО ОТПРАВЛЕНО УСПЕШНО ВЫВОДИМ СООБЩЕНИЕ*/
+        if ($send == "true")
+        {
+            echo "Спасибо! Ваше сообщение отправлено.\r\n";
+        }
+        /*ЕСЛИ ПИСЬМО НЕ УДАЛОСЬ ОТПРАВИТЬ ВЫВОДИМ СООБЩЕНИЕ ОБ ОШИБКЕ*/
+        else
+        {
+            echo "Не удалось отправить, попробуйте снова!";
+        }
+    }
+
 ?>
--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,55 @@
 let gulp = require('gulp'),
-	sass = require('gulp-sass')(require('sass'));
-	browserSync = require('browser-sync'),
-	uglify = require('gulp-uglify'),
-	concat = require('gulp-concat'),
-	rename = require('gulp-rename'),
-	del = require('del'),
-	autoprefixer = require('gulp-autoprefixer');
+    sass = require('gulp-sass')(require('sass')),
+    browserSync = require('browser-sync'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    del = require('del'),
+    autoprefixer = require('gulp-autoprefixer');
 
-
+// Очистка папки docs
 gulp.task('clean', async function(){
-	del.sync('docs')
-})
+    await del('docs');
+});
 
+// Компиляция Sass в CSS
 gulp.task('sass', function(){
-	return gulp.src('app/sass/**/*.sass')
-		.pipe(sass({outputStyle: 'compressed'}))
-		.pipe(autoprefixer({
-			overrideBrowserslist:  ['last 8 versions']
-		}))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('app/css'))
-		.pipe(browserSync.reload({stream: true}))
+    return gulp.src('app/sass/**/*.sass')
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(autoprefixer({
+            overrideBrowserslist:  ['last 8 versions']
+        }))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream: true}));
 });
 
-
+// Обработка стилей (нормализация и сторонние библиотеки)
 gulp.task('css', function(){
-	return gulp.src([
-		'node_modules/normalize.css/normalize.css',
-		'node_modules/slick-carousel/slick/slick.css'
-		])
-	.pipe(sass({outputStyle: 'compressed'}))
-	.pipe(concat('libs.css'))
-	.pipe(rename({suffix: '.min'}))
-	.pipe(gulp.dest('app/css'))
-	.pipe(browserSync.reload({stream: true}))
-})
+    return gulp.src([
+        'node_modules/normalize.css/normalize.css',
+        'node_modules/slick-carousel/slick/slick.css'
+    ])
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(concat('libs.css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream: true}));
+});
 
+// Обновление HTML файлов
 gulp.task('html', function(){
-	return gulp.src('app/*.html')
-	.pipe(browserSync.reload({stream: true}))
-
+    return gulp.src('app/*.html')
+        .pipe(browserSync.reload({stream: true}));
 });
 
+// Обновление JS файлов
 gulp.task('script', function(){
-	return gulp.src('app/js/*.js')
-	.pipe(browserSync.reload({stream: true}))
-
+    return gulp.src('app/js/*.js')
+        .pipe(browserSync.reload({stream: true}));
 });
 
-
-
+// Настройка BrowserSync для живой перезагрузки
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
@@ -58,30 +58,35 @@ gulp.task('browser-sync', function() {
     });
 });
 
+// Экспорт всех файлов в папку docs
 gulp.task('export', function(){
-	let buildHtml = gulp.src('app/**/*.html')
-		.pipe(gulp.dest('docs'));
+    return Promise.all([
+        gulp.src('app/**/*.html')
+            .pipe(gulp.dest('docs')),
 
-	let BuildCss = gulp.src('app/css/**/*.css')
-	.pipe(gulp.dest('docs/css'));
+        gulp.src('app/css/**/*.css')
+            .pipe(gulp.dest('docs/css')),
 
-	let BuildJs = gulp.src('app/js/**/*.js')
-	.pipe(gulp.dest('docs/js'));
+        gulp.src('app/js/**/*.js')
+            .pipe(gulp.dest('docs/js')),
 
-	let BuildFonts = gulp.src('app/fonts/**/*.*')
-	.pipe(gulp.dest('docs/fonts'));
+        gulp.src('app/fonts/**/*.*')
+            .pipe(gulp.dest('docs/fonts')),
 
-
-	let BuildImg = gulp.src('app/img/**/*.*')
-	.pipe(gulp.dest('docs/img'));
+        gulp.src('app/img/**/*.*')
+            .pipe(gulp.dest('docs/img'))
+    ]);
 });
 
+// Следим за изменениями файлов
 gulp.task('watch', function(){
-	gulp.watch('app/sass/**/*.sass', gulp.parallel('sass'));
-	gulp.watch('app/*.html', gulp.parallel('html'));
-	gulp.watch('app/js/*.js',gulp.parallel('script'));
+    gulp.watch('app/sass/**/*.sass', gulp.parallel('sass'));
+    gulp.watch('app/*.html', gulp.parallel('html'));
+    gulp.watch('app/js/*.js', gulp.parallel('script'));
 });
 
+// Задача для билда (с очисткой и экспортом)
 gulp.task('build', gulp.series('clean', 'export'));
 
-gulp.task('default', gulp.parallel('css', 'sass', 'browser-sync', 'watch'))
+// Основная задача для запуска
+gulp.task('default', gulp.parallel('css', 'sass', 'browser-sync', 'watch'));
